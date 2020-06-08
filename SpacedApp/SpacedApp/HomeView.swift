e1252e5aa7708c1c735cf78944d2686b0e7153ad
Anderson <anderlentz@gmail.com>
2020-06-08 13:42:03 -0300

    Refactor view components

--- SpacedApp/SpacedApp/HomeView.swift
+++ SpacedApp/SpacedApp/HomeView.swift
@@ -9,13 +9,13 @@
 import SwiftUI
 
 struct HomeView: View {
-    
+
     @State var trendingItems:[TrendingItem] = [
         TrendingItem(imageName: "roberto-nickson-g-549146-unsplash", title: "Ophiuchi", subtitle: "KADUNA", buttonTitle: "0.4 BTC"),
         TrendingItem(imageName: "kenny-luo-516116-unsplash", title: "Ophiuchi", subtitle: "KADUNA", buttonTitle: "0.4 BTC"),
         TrendingItem(imageName: "simone-hutsch-699861-unsplash", title: "Ophiuchi", subtitle: "KADUNA", buttonTitle: "0.4 BTC")
     ]
-    
+
     @State var x: CGFloat = 30
     @State var count: Int = 0
     @State var screen = UIScreen.main.bounds.width - 60
@@ -25,113 +25,105 @@ struct HomeView: View {
     }
 
     var body: some View {
-        VStack() {
-            header
-        }
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-        .background(Color.black)
-    }
-        
-    private var header: some View {
-        TabView {
-            ZStack(alignment: .top) {
-            
-                GeometryReader { geometry in
 
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text("Discover")
-                                .foregroundColor(.white)
-                                .font(.system(size: 34))
-                                .bold()
-
-                            Spacer()
-
-                            Profile(image: "kal-loftus-unsplash", style: .small)
-
+        let drag = DragGesture(minimumDistance: 50, coordinateSpace: .local)
+                    .onChanged({ value in
+                        if value.translation.width > 0 {
+                            self.x = value.location.x
+                        } else {
+                            self.x = value.location.x - self.screen
                         }
-                        HStack {
-                            Text("Trending locations today")
-                                .foregroundColor(Color(red: 213/255, green: 217/255, blue: 223/255))
-                                .font(.system(size: 16))
+                    })
+                    .onEnded({ value in
+                        if value.translation.width > 0 {
+                            if value.translation.width > ((self.screen) / 2) &&
+                                Int(self.count) != 0 {
+                                self.count -= 1
+
+                                if self.count == 0 { //if first card
+                                   self.x = 30 // same padding horizontal of  Discover
+                                } else {
+                                    self.x = -((self.screen - 20) * CGFloat(self.count))
+                                }
+                            } else {
+                                self.x = -((self.screen - 20) * CGFloat(self.count))
+                            }
+                        } else {
+                            if -value.translation.width > ((self.screen) / 2) &&
+                                Int(self.count) != (self.trendingItems.count - 1) {
+                                self.count += 1
+
+                                if self.count == self.trendingItems.count - 1 { //last element
+                                    self.x = -((self.screen - 5) * CGFloat(self.count))
+                                } else {
+                                    self.x = -((self.screen - 20) * CGFloat(self.count))
+                                }
+                            } else {
+                                // if here user tried to scroll last card
+                                self.x = -((self.screen - 5) * CGFloat(self.count))
+                            }
                         }
+                    })
+
+            return TabView {
+                ZStack(alignment: .top) {
+                    ScrollView {
+                        GeometryReader { geometry in
+                            self.header
+                                .padding(.top, 56)
+                                .padding(.horizontal, 30)
+
+                            self.carouselView
+                                .highPriorityGesture(drag)
+                                .animation(.spring(response: 0.8, dampingFraction: 0.7, blendDuration: 0.5))
+                                .padding(.top, 147)
                     }
-
-                    .padding(.top, 56)
-                    .padding(.horizontal, 30)
-
-                    HStack(spacing: 10) {
-                        ForEach((0...2), id: \.self) {
-                            TrendingCard(trendingItem: self.trendingItems[$0],size: CGSize(width:self.screen,height: 372))
-                                .scaleEffect($0 == self.count ? 1 : 0.96)
-                                .offset(x: self.x)
-                                .highPriorityGesture(DragGesture()
-                                    .onChanged({ value in
-                                        if value.translation.width > 0 {
-                                            self.x = value.location.x
-                                        } else {
-                                            self.x = value.location.x - self.screen
-                                        }
-                                    })
-                                    .onEnded({ value in
-                                        if value.translation.width > 0 {
-                                            if value.translation.width > ((self.screen) / 2) &&
-                                                Int(self.count) != 0 {
-                                                self.count -= 1
-
-                                                if self.count == 0 { //if first card
-                                                   self.x = 30 // same padding horizontal of  Discover
-                                                } else {
-                                                    self.x = -((self.screen - 20) * CGFloat(self.count))
-                                                }
-                                            } else {
-                                                self.x = -((self.screen - 20) * CGFloat(self.count))
-                                            }
-                                        } else {
-                                            if -value.translation.width > ((self.screen) / 2) &&
-                                                Int(self.count) != (self.trendingItems.count - 1) {
-                                                self.count += 1
-
-                                                if self.count == self.trendingItems.count - 1 { //last element
-                                                    self.x = -((self.screen - 5) * CGFloat(self.count))
-                                                } else {
-                                                    self.x = -((self.screen - 20) * CGFloat(self.count))
-                                                }
-                                            } else {
-                                                // if here user tried to scroll last card
-                                                self.x = -((self.screen - 5) * CGFloat(self.count))
-                                            }
-                                        }
-                                    })
-                                )
-                        }
-                    }
-                    .animation(.spring(response: 0.8, dampingFraction: 0.7, blendDuration: 0.5))
-                    .padding(.top, 147)
-
-
                 }
             }
+                .highPriorityGesture(drag)
                 .background(Color.black)
                 .edgesIgnoringSafeArea([.top, .bottom])
-                .tabItem {
-                    Image("Home")
-                }
+                .tabItem { Image("Home") }
 
             FakeView1()
-                .tabItem {
-                     Image("location")
+                .tabItem { Image("location") }
+
+            FakeView1()
+                .tabItem { Image("Search") }
+
+            FakeView1()
+                .tabItem { Image("Account") }
+        }
+    }
+
+    private var carouselView: some View {
+        HStack(spacing: 10) {
+            ForEach((0...2), id: \.self) {
+                TrendingCard(trendingItem: self.trendingItems[$0],size: CGSize(width:self.screen,height: 372))
+                    .scaleEffect($0 == self.count ? 1 : 0.96)
+                    .offset(x: self.x)
             }
+        }
+    }
 
-            FakeView1()
-                .tabItem {
-                     Image("Search")
-                }
+    private var header: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Discover")
+                    .foregroundColor(.white)
+                    .font(.system(size: 34))
+                    .bold()
 
-            FakeView1()
-                .tabItem {
-                     Image("Account")
-                }
+                Spacer()
+
+                Profile(image: "kal-loftus-unsplash", style: .small)
+
+            }
+            HStack {
+                Text("Trending locations today")
+                    .foregroundColor(Color(red: 213/255, green: 217/255, blue: 223/255))
+                    .font(.system(size: 16))
+            }
         }
     }
 
@@ -141,18 +133,6 @@ struct HomeView: View {
         }
     }
 
-    struct FakeView2: View {
-        var body: some View {
-            Color.black
-        }
-    }
-
-    struct FakeView3: View {
-        var body: some View {
-            Color.black
-        }
-    }
-    
     func getMid() -> Int {
         return trendingItems.count / 2
     }
@@ -162,5 +142,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
     }
-    
+
 }
