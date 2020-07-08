201e221e98b825a6add2e511f3b444a2cc9b9807
Anderson <anderlentz@gmail.com>
2020-07-08 20:39:01 -0300

    Refactor HomeView to use List

--- SpacedApp/SpacedApp/HomeView.swift
+++ SpacedApp/SpacedApp/HomeView.swift
@@ -25,94 +25,88 @@ struct HomeView: View {
     }
 
     var body: some View {
+        TabView {
+            VStack(alignment: .leading) {
 
-        let drag = DragGesture(minimumDistance: 50, coordinateSpace: .local)
-                    .onChanged({ value in
-                        if value.translation.width > 0 {
-                            self.x = value.location.x
-                        } else {
-                            self.x = value.location.x - self.screen
-                        }
-                    })
-                    .onEnded({ value in
-                        if value.translation.width > 0 {
-                            if value.translation.width > ((self.screen) / 2) &&
-                                Int(self.count) != 0 {
-                                self.count -= 1
+                self.header
+                    .padding(.horizontal, 15)
 
-                                if self.count == 0 { //if first card
-                                   self.x = 30 // same padding horizontal of  Discover
-                                } else {
-                                    self.x = -((self.screen - 20) * CGFloat(self.count))
-                                }
-                            } else {
-                                self.x = -((self.screen - 20) * CGFloat(self.count))
-                            }
-                        } else {
-                            if -value.translation.width > ((self.screen) / 2) &&
-                                Int(self.count) != (self.trendingItems.count - 1) {
-                                self.count += 1
-
-                                if self.count == self.trendingItems.count - 1 { //last element
-                                    self.x = -((self.screen - 5) * CGFloat(self.count))
-                                } else {
-                                    self.x = -((self.screen - 20) * CGFloat(self.count))
-                                }
-                            } else {
-                                // if here user tried to scroll last card
-                                self.x = -((self.screen - 5) * CGFloat(self.count))
-                            }
-                        }
-                    })
-
-            return TabView {
-                ZStack(alignment: .top) {
-                    ScrollView {
-                        GeometryReader { geometry in
-                            self.header
-                                .padding(.top, 56)
-                                .padding(.horizontal, 30)
-
-                            self.carouselView
-                                .highPriorityGesture(drag)
-                                .animation(.spring(response: 0.8, dampingFraction: 0.7, blendDuration: 0.5))
-                                .padding(.top, 135)
-
-                            self.innerTitle
-                                .padding(.top, 527)
-                                .padding(.leading, 30)
-                                .fixedSize(horizontal: false, vertical: true)
-
-                            self.trendingCategories
-                                .padding(.top, 625)
-                                .padding(.horizontal, 30)
-                    }
+                List {
+                    self.carouselView
+                    self.feelingAdventurousRow
                 }
-            }
-                .highPriorityGesture(drag)
-                .background(Color.black)
-                .edgesIgnoringSafeArea([.top, .bottom])
-                .tabItem { Image("Home") }
+                .listStyle(PlainListStyle())
 
-            FakeView1()
-                .tabItem { Image("location") }
+        }
+            .edgesIgnoringSafeArea([.bottom])
+            .tabItem { Image("Home") }
 
-            FakeView1()
-                .tabItem { Image("Search") }
+        FakeView1()
+            .tabItem { Image("location") }
 
-            FakeView1()
-                .tabItem { Image("Account") }
+        FakeView1()
+            .tabItem { Image("Search") }
+
+        FakeView1()
+            .tabItem { Image("Account") }
+        }
+
+        .preferredColorScheme(.dark)
+        .onAppear {
+            UITableView.appearance().separatorStyle = .none 
         }
     }
-
     private var carouselView: some View {
-        HStack(spacing: 10) {
-            ForEach((0...2), id: \.self) {
-                TrendingCard(trendingItem: self.trendingItems[$0],size: CGSize(width:self.screen,height: 372))
-                    .scaleEffect($0 == self.count ? 1 : 0.96)
-                    .offset(x: self.x)
+        let drag = DragGesture(minimumDistance: 60, coordinateSpace: .local)
+        .onChanged({ value in
+            if value.translation.width > 0 {
+                self.x = value.location.x
+            } else {
+                self.x = value.location.x - self.screen
             }
-        }
+        })
+        .onEnded({ value in
+            if value.translation.width > 0 {
+                if value.translation.width > ((self.screen) / 2) &&
+                    Int(self.count) != 0 {
+                    self.count -= 1
+
+                    if self.count == 0 { //if first card
+                       self.x = 0 // same padding horizontal of  Discover
+                    } else {
+                        self.x = -((self.screen - 20) * CGFloat(self.count))
+                    }
+                } else {
+                    self.x = -((self.screen - 20) * CGFloat(self.count))
+                }
+            } else {
+                if -value.translation.width > ((self.screen) / 2) &&
+                    Int(self.count) != (self.trendingItems.count - 1) {
+                    self.count += 1
+
+                    if self.count == self.trendingItems.count - 1 { //last element
+                        self.x = -((self.screen - 5) * CGFloat(self.count))
+                    } else {
+                        self.x = -((self.screen - 20) * CGFloat(self.count))
+                    }
+                } else {
+                    // if here user tried to scroll last card
+                    self.x = -((self.screen - 5) * CGFloat(self.count))
+                }
+            }
+        })
+
+        return (
+            HStack(spacing: 10) {
+                ForEach((0...2), id: \.self) { count in
+                    TrendingCard(trendingItem: self.trendingItems[count],size: CGSize(width:self.screen,height: 372))
+                    .scaleEffect(count == self.count ? 1 : 0.96)
+                    .offset(x: self.x)
+                }
+            }
+            .highPriorityGesture(drag)
+            .animation(.spring(response: 0.8, dampingFraction: 0.7, blendDuration: 0.5))
+        )
     }
 
     private var header: some View {
@@ -148,53 +142,73 @@ struct HomeView: View {
         }
     }
 
-    private var trendingCategories: some View {
-        GeometryReader {  geometry in
-            VStack(alignment: .center, spacing: 10) {
-                HStack(alignment: .center,spacing: 10) {
-                    Image("nature")
-                        .resizable()
-                        .frame(width: geometry.size.width/2 - 5, height: 80)
-                        .overlay(
-                            Text("Nature")
-                                .foregroundColor(.white)
-                                .fontWeight(.semibold)
-                                .font(.system(size: 18))
-                        )
-                    Image("futuristic")
-                        .resizable()
-                        .frame(width: geometry.size.width/2 - 5, height: 80)
-                        .overlay(
-                            Text("Futuristic")
-                                .foregroundColor(.white)
-                                .fontWeight(.semibold)
-                                .font(.system(size: 18))
-                        )
-                }
-                HStack(alignment: .center,spacing: 10) {
-                    Image("party")
-                        .resizable()
-                        .frame(width: geometry.size.width/2 - 5, height: 80)
-                        .overlay(
-                            Text("Party")
-                                .foregroundColor(.white)
-                                .fontWeight(.semibold)
-                                .font(.system(size: 18))
-                        )
-                    Image("green")
-                        .resizable()
-                        .frame(width: geometry.size.width/2 - 5, height: 80)
-                        .overlay(
-                            Text("Green")
-                                .foregroundColor(.white)
-                                .fontWeight(.semibold)
-                                .font(.system(size: 18))
-                        )
-               }
+    private var feelingAdventurousRow: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(alignment: .leading) {
+                self.innerTitle
+                    .frame(minWidth: 0, minHeight: 0, maxHeight: 200)
+                self.trendingCategories
+                    .frame(minWidth: 0, minHeight: 200, maxHeight: 200)
             }
-            .frame(minWidth: geometry.size.width)
+                .background(Color.black)
+                .edgesIgnoringSafeArea(.all)
         }
     }
+
+    private var trendingCategories: some View {
+        VStack(alignment: .center, spacing: 10) {
+            HStack(alignment: .center,spacing: 10) {
+                Image("nature")
+                    .resizable()
+                    .overlay(
+                        Text("Nature")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .font(.system(size: 18))
+                    )
+                    .onTapGesture {
+                        print("Nature")
+                    }
+                Image("futuristic")
+                    .resizable()
+                    .overlay(
+                        Text("Futuristic")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .font(.system(size: 18))
+                    )
+                    .onTapGesture {
+                        print("Futuristic")
+                    }
+            }
+            HStack(alignment: .center,spacing: 10) {
+                Image("party")
+                    .resizable()
+                    .overlay(
+                        Text("Party")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .font(.system(size: 18))
+                    )
+                    .onTapGesture {
+                        print("Party")
+                    }
+                Image("green")
+                    .resizable()
+                    .overlay(
+                        Text("Green")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .font(.system(size: 18))
+                    )
+                    .onTapGesture {
+                        print("Green")
+                    }
+            }
+        }
+    }
+
     struct FakeView1: View {
         var body: some View {
             Color.black
